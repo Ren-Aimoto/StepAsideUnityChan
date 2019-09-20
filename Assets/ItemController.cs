5821969d2e81905547102b91788c4c2e5ea0a6f6
相本廉 <ren.aimoto3.6@gmail.com>
2019-09-20 11:01:23 +0900

    StepAsideUnityChanプロジェクト　初めてのコミット
    発展課題まで終了済みの物をコミットしています。

---
+++ Assets/ItemController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemController : MonoBehaviour {
+	GameObject CameraPos;
+	// Use this for initialization
+	void Start () {
+		this.CameraPos = GameObject.Find ("Main Camera");
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (this.CameraPos.transform.position.z > gameObject.transform.position.z) {
+			Destroy (gameObject);
+		}
+	}
+}
